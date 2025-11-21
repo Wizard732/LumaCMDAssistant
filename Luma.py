e4ad7bdf1d327c69112695a1d961e36398355321
Maksym Stetsenko <mxm.stecenko@gmail.com>
2025-11-21 09:15:19 +0200

    Add TODOs comments

--- Luma.py
+++ Luma.py
@@ -5,11 +5,21 @@ import webbrowser
 from shutdown import shutdown, restart
 from pyautogui import hotkey
 
+# TODO:
+# General comments:
+# 1) Move "opps" to a separate json file with name "configs.json" and read these configs from here. Do not aks user actually to modify the code (bad practice because User can accidentally break the code).
+# 2) Transalte everything to English
+# 3) Create "requirements.txt" file and use it to install all required dependencies.
+# 4) Use virtual envs in Python
+# 5) Translate README.md to English as well
+
+
+
 opps = {
     "name":{"Лум","Лим","Лем","Лумс","Lum","Luma","Lumas","Лума",},
     "command":{"Включи","Выключи","Запусти","Закрой","Отключи","Выйди","Открой","Ливни",},
     "apps": {
-
+      # TODO: If User should modify only these "apps" section then move only this section to "configs.json"
         "steam": r"C:\Program Files (x86)\Steam\steam.exe",
         "стим": r"C:\Program Files (x86)\Steam\steam.exe",
 
@@ -61,6 +71,7 @@ def StartProgram(cmd):
         else:
             speak("Приложение не найдено.")
 
+# TODO: rename to "TryExecuteCommand" for more obvious function purpose
 def Command(cmd):
       text = cmd.lower().strip()
 
